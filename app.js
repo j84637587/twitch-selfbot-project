2665f49620cc213fe22f46e5128a8141de696998
Quareta <j84637587@gmail.com>
2022-07-18 20:06:42 +0800

    fix: Resolve promise error on setTimeout

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const config = require("config");
 const channels = config.get("CHANNELS");
 const spam_channels = config.get("SPAM_CHANNELS");
 const channel_ids = Object.keys(channels);
-const RegExps = {};
+let RegExps = {};
 
 const client = new tmi.Client({
   options: {
@@ -30,6 +30,9 @@ client.on("connected", (adress, port) => {
   let spamIntervalID = setInterval(sendEmoji, 3 * 61 * 1_000);
 });
 
+/**
+ * Init regex data
+ */
 module.exports.init = function () {
   for (const ch in channels) {
     if (!channels[ch].hasOwnProperty("extra")) continue;
@@ -55,7 +58,7 @@ function sendEmoji() {
 }
 
 client.on("message", async (channel, tags, message, self) => {
-  console.log(message)
+  // console.log(message); // this would print all message we recevice
   for (const ch in channels) {
     if (channel.substring(1) == ch) {
       let response = this.matchMessage(ch, message);
@@ -68,27 +71,6 @@ client.on("message", async (channel, tags, message, self) => {
           console.log(`\u001b[31m${ch}: ${response}\u001b[0m`);
         }
       }
-      // for (const e in RegExps[ch]) {
-      //   // regex match
-      //   let match = RegExps[ch][e].exec(message);
-      //   if (match == null) continue;
-
-      //   // get username
-      //   let index = match.length;
-      //   while (index-- && !match[index]);
-      //   if (index == -1) break;
-
-      //   // make response
-      //   let response = channels[ch]["extra"][e]["response"];
-      //   response = response.replace("%username%", match[index]);
-      //   response = response.replace("%emote_here%", channels[ch]["emoji"]);
-      //   if (channels[ch]["response"] == true) {
-      //     client.say(channel, response);
-      //     console.log(`\u001b[32m${ch}: ${response}\u001b[0m`);
-      //   } else {
-      //     console.log(`\u001b[31m${ch}: ${response}\u001b[0m`);
-      //   }
-      // }
       break;
     }
   }
@@ -115,7 +97,7 @@ module.exports.matchMessage = function (channel, message) {
 
     // make response
     let response = channels[channel]["extra"][e]["response"];
-    console.log(">", channel, channels[channel], channels[channel]["extra"], channels[channel]["extra"][e])
+    // console.log(">", channel, channels[channel], channels[channel]["extra"], channels[channel]["extra"][e])
     response = response.replace("%username%", match[index]);
     response = response.replace("%emote_here%", channels[channel]["emoji"]);
     return response;
@@ -125,38 +107,24 @@ module.exports.matchMessage = function (channel, message) {
 
 client.on("cheer", (channel, userstate, message) => {
   let bits_count = ~~userstate["bits"];
-  console.log(`cheer with ${senderCount} bits.`);
+  console.log(`\u001b[33m ${userstate["display-name"]} cheer with ${bits_count} bits. \u001b[0m`); // print bits amount
   let response = `@${userstate["display-name"]} 感謝小奇點 %emote_here% `;
+
+  // add extra message when reach specific amount of bits
   if (bits_count >= 500) {
     response += "777";
   }
   sayFormatResponse(channel, response);
 });
 
-/**
- * Raided
- * Channel is now being raided by another broadcaster.
- *
- * channel {string} Channel name being raided
- * username {string} - Username raiding the channel
- * viewers {integer} - Viewers count
- */
 client.on("raided", (channel, username, viewers) => {
   let response = `@${username} 感謝揪團 %emote_here% 各位可以點擊主播的頭像兩下或是F5降落喔~ 觀眾也可以去追隨 @${username} 喔!`;
   sayFormatResponse(channel, response);
 });
 
-/**
- * Username gifted a subscription to recipient in a channel.
- *
- * channel {string} Channel name
- * username {string} Sender username
- * recipient {string} Recipient username
- */
 client.on(
   "subgift",
   (channel, username, streakMonths, recipient, methods, userstate) => {
-    console.log(`subgift by ${username} with ${senderCount} gifts.`);
     console.log(`${recipient}`);
     console.log(`${userstate}`);
     let response = `@${username} 感謝贈送訂閱 %emote_here% `;
@@ -174,13 +142,21 @@ client.on("resub", (channel, username, method, message, userstate) => {
   sayFormatResponse(channel, response);
 });
 
+/**
+ * Formate response and send to channel.
+ * 
+ * @param {string} channel   Channel to send
+ * @param {string} response  Response message
+ */
 function sayFormatResponse(channel, response) {
   for (const ch in channels) {
     // console.log(ch, channels[ch], channel.substring(1))
     if (channel.substring(1) == ch) {
       response = response.replace("%emote_here%", channels[ch]["emoji"]);
       if (channels[ch]["response"] == true) {
-        setTimeout(client.say, 5 * 1_000, channel, response);
+        setTimeout(function () {
+          client.say(channel, response);
+        }, 5 * 1_000);
         console.log(`\u001b[32m${ch}: ${response}\u001b[0m`);
       } else {
         console.log(`\u001b[31m${ch}: ${response}\u001b[0m`);
